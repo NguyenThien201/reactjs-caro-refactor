a36853afa26e5282cf3a8fc265cabc6eed3fe1d6
thiennguyen <thien.nguyen@nexpando.com>
2021-10-27 00:42:10 +0700

    add game

---
+++ src/components/board.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import Square from "./square";
+
+const Board = ({squares, poses, winner, size, onClick}) => {
+    const renderSquare = (i) => {
+      return (
+        <Square
+          value={squares[i]}
+          hightLight={
+            i === poses[poses.length - 1] ||
+            winner.includes(i)
+          }
+          onClick={() => onClick(i)}
+        />
+      );
+    }
+    const renderRow = (rowIndex, items) => {    
+      var row = [];
+      for (var i = 0; i < items; i++) {
+        let cell = renderSquare(rowIndex * items + i);
+        row.push(cell);
+      }
+      return <div className="board-row">{row}</div>;
+    }
+  
+    const renderBoard = (row, col) => {        
+        console.log(row)
+        console.log(col)
+      var board = [];
+      for (var i = 0; i < row; i++) {
+        let row = renderRow(i, col);
+        board.push(row);
+      }
+      return board;
+    }
+  
+    
+    return (
+        <>        
+        <div>{renderBoard(size, size)}</div>
+        </>
+    
+    );
+    
+  }
+  
+  export default Board;
