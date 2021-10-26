44f4c14c44c2802f1227bb05c220002d5b0b9a20
thiennguyen <thien.nguyen@nexpando.com>
2021-10-27 00:44:28 +0700

    update file

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import ReactDOM from "react-dom";
 
 import "./index.css";
 
-import Board from './components/board';
+import Board from './board';
+
 const Game = () => {
 
   const [history, setHistory] = useState([
